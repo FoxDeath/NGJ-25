f2705941c713d0bc34d2aa60b3fdcfa1ba29b588
FoxDeath <matusaradu@gmail.com>
2025-04-06 12:25:48 +0200

    Buttons sprites

--- Assets/Scripts/Enemies/ButtonDrop.cs
+++ Assets/Scripts/Enemies/ButtonDrop.cs
@@ -1,14 +1,21 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ButtonDrop : MonoBehaviour
 {
+    [SerializeField] List<Sprite> sprites;
+    
     private GameController gameController;
     private int attributesReward;
 
     private void Start()
     {
         gameController = FindAnyObjectByType<GameController>();
+        
+        // Set a random sprite from the list
+        int randomIndex = UnityEngine.Random.Range(0, sprites.Count);
+        GetComponent<SpriteRenderer>().sprite = sprites[randomIndex];
     }
 
     private void OnTriggerEnter(Collider other)
